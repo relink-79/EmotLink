70920302ce55bb807d1f425555f5fd5f4d0c6844
GoraniPie <intelpieservice@gmail.com>
2025-07-16 23:05:11 +0900

    [GoraniPie] Add gitignore & Fix Android Studio Env

--- android/app/src/main/java/com/emotlink/app/MainActivity.java
+++ android/app/src/main/java/com/emotlink/app/MainActivity.java
@@ -1,5 +1,26 @@
 package com.emotlink.app;
 
+import android.os.Bundle;
+import android.webkit.*;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+
+        webView.setWebViewClient(new WebViewClient());
+        var webViewSettings = webView.getSettings();
+        webViewSettings.setJavaScriptEnabled(true);
+        webViewSettings.setUseWideViewPort(true);
+        webView.getSettings().setBuiltInZoomControls(false);
+        webView.getSettings().setSupportZoom(false);
+
+
+        webView.loadUrl("http://10.0.2.2:8000"); // localhost
+    }
+
+}
